02aab66bf9ddb733fcaccf7c89581493efb0a93c
maphy9 <maphy9@gmail.com>
2024-08-13 08:23:37 +0200

    transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ local function config()
 			light = "latte",
 			dark = "mocha",
 		},
-		transparent_background = false,
+		transparent_background = true,
 		show_end_of_buffer = false,
 		term_colors = false,
 		dim_inactive = {
