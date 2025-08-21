3a3a0334d5a7e04cdce96a93e48bca60bd33a1f8
maphy9 <maphy9@gmail.com>
2025-08-21 22:48:09 +0200

    Add go in treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
-        ensure_installed = { "lua", "python", "javascript", "typescript", "rust" },
+        ensure_installed = { "lua", "python", "javascript", "typescript", "rust", "go" },
         sync_install = false,
         auto_install = true,
         ignore_install = { "javascript" },
